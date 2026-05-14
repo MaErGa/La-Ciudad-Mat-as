1126995ba68cce7eedff9663ec87a270d9c3b8c8
RetroMetalGamer <matiaserrigar87@gmail.com>
2026-05-14 14:09:00 +0100

    actualizacion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movimiento")]
+    public float maxSpeed = 25f;
+    public float acceleration = 8f;
+    public float brakeForce = 12f;
+    public float turnSpeed = 100f;
+    [Header("Físicas")]
+    public float drag = 2f;
+    public float downforce = 50f;
+    private Rigidbody rb;
+    private float currentSpeed;
+    private float inputVertical;
+    private float inputHorizontal;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0, -0.5f, 0); // Estabilidad
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        inputVertical = Input.GetAxis("Vertical");
+        inputHorizontal = Input.GetAxis("Horizontal");
     }
+    void FixedUpdate()
+    {
+        // Aceleración y freno
+        if (inputVertical > 0)
+        {
+            currentSpeed += acceleration * inputVertical * Time.fixedDeltaTime;
+        }
+        else if (inputVertical < 0)
+        {
+            currentSpeed += brakeForce * inputVertical * Time.fixedDeltaTime;
+        }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0, drag * Time.fixedDeltaTime);
+        }
+        currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeed * 0.4f, maxSpeed);
+        // Movimiento hacia adelante
+        Vector3 velocity = transform.forward * currentSpeed;
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        // Giro (solo si hay velocidad)
+        if (Mathf.Abs(currentSpeed) > 0.5f)
+        {
+            float turnAmount = inputHorizontal * turnSpeed * Time.fixedDeltaTime;
+            turnAmount *= Mathf.Sign(currentSpeed); // Invertir giro en reversa
+            Quaternion turnRotation = Quaternion.Euler(0, turnAmount, 0);
+            rb.MoveRotation(rb.rotation * turnRotation);
+        }
+        // Downforce para estabilidad
+        rb.AddForce(-transform.up * downforce * rb.velocity.magnitude);
+    }
+    public Vector3 GetVelocity() => transform.forward * currentSpeed;
+    public float GetSpeed() => currentSpeed;
 }
